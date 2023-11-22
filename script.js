67c6d1e7556bc2d007a933c4101565c3185a4576
soonduck <soonduck.web@gmail.com>
2023-11-22 15:52:07 +0900

    UPDATE: implemente leftDeck & shuffle

--- script.js
+++ script.js
@@ -14,10 +14,12 @@ const area = {
   area5: [],
   openIndex5: 5,
   area6: [],
-  openIndex6: 6
+  openIndex6: 6,
 };
 
 // 전체 덱
+
+// prettier-ignore
 let deck = [
   'SA', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8', 'S9', 'S10', 'SJ', 'SQ', 'SK',
   'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK',
@@ -31,68 +33,88 @@ let leftDeck = [];
 let openLeftDeck = [];
 
 let sidePattern = {
-  S: '',
-  H: '',
-  D: '',
-  C: ''
-}
+  S: "",
+  H: "",
+  D: "",
+  C: "",
+};
 
+// prettier-ignore
 const cardNum = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 // 함수
 
 const findImg = (card) => {
-  return 'img/' + card + '.svg'
-}
-
-const randomizeDeck = () => {
-  // ??
-}
+  return "img/" + card + ".svg";
+};
 
 // deck 카드를 다른 변수(덱)에 넣어주는 함수
 const spreadCards = () => {
-  let startIdx = 0
+  // area
+  let startIdx = 0;
   for (let i = 0; i < 7; i++) {
-    const endIdx = startIdx + i + 1
-    area['area' + i] = deck.slice(startIdx, endIdx);
-    startIdx = endIdx
+    const endIdx = startIdx + i + 1;
+    area["area" + i] = deck.slice(startIdx, endIdx);
+    startIdx = endIdx;
   }
-}
+  // leftDeck
+  leftDeck = deck.slice(28);
+};
+
+const shuffleDeck = (target) => {
+  for (let i = 0; i < target.length; i++) {
+    const j = Math.floor(Math.random() * target.length);
+    [target[i], target[j]] = [target[j], target[i]];
+  }
+};
+
+const drawThreeCards = () => {
+  if (!leftDeck.length) {
+    leftDeck = openLeftDeck;
+    openLeftDeck = [];
+    shuffleDeck(leftDeck); // left deck shuffle
+  } else openLeftDeck = [...openLeftDeck, ...leftDeck.splice(0, 3)];
+  console.log(openLeftDeck);
+};
 
 // 카드 렌더 함수
 
 const renderWholeCards = () => {
-  renderAreas(0,1,2,3,4,5,6)
-}
+  renderAreas(0, 1, 2, 3, 4, 5, 6);
+  // Left Deck Event
+  const $deck = document.querySelector(".deck");
+  $deck.onclick = drawThreeCards;
+};
 
 const renderAreas = (...areaIdx) => {
   const renderArea = (idx) => {
-    const $area = document.querySelector('.center-area' + idx)
-    let gap = 17
-
+    const $area = document.querySelector(".center-area" + idx);
+    let gap = 0;
     for (let i = 0; i < idx + 1; i++) {
-      const card = area['area'+idx][i]
-      const $img = document.createElement('img')
-      $img.src = i >= area['openIndex'+idx] ? findImg(card) : 'img/backward_orange.svg'
-      $img.style.top = gap + 'px'
-      $img.style.zIndex = 'i + 1'
-      gap += 17
-      $area.appendChild($img)
+      const card = area["area" + idx][i];
+      const $img = document.createElement("img");
+      $img.src =
+        i >= area["openIndex" + idx]
+          ? findImg(card)
+          : "img/backward_orange.svg";
+      $img.style.top = gap + "px";
+      $img.style.zIndex = "i + 1";
+      gap += 17;
+      $area.appendChild($img);
     }
-  }
-
+  };
   for (let i = 0; i < areaIdx.length; i++) {
-    renderArea(areaIdx[i])
+    renderArea(areaIdx[i]);
   }
-}
+};
 
 // 렌더
 
 const render = () => {
-  randomizeDeck();
+  shuffleDeck(deck);
   spreadCards();
-  renderWholeCards()
-  console.log(area)
-}
+  renderWholeCards();
+  console.log(area);
+};
 
 render();
